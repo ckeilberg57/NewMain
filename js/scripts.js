13771fb1e701bf635beb57d9c5a5731f0310fbd3
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2024-04-23 14:26:46 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -375,7 +375,7 @@
        
       // Add event listeners to the buttons
       rightBox.querySelector('.customButton1').addEventListener('click', function() {
-        openVideo('https://youtu.be/J6Ag9gXBEEA');
+        openVideo('https://youtu.be/embed/J6Ag9gXBEEA');
       });
 
       rightBox.querySelector('.customButton2').addEventListener('click', function() {
